0952a916a2e312a29a89c9b966491380a4086dc0
phi <phi1618jp@gmail.com>
2017-01-31 10:26:54 +0900

    setup gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+var rename = require("gulp-rename"); 
+var cssmin = require('gulp-cssmin');
+
+gulp.task('less', function() {
+  gulp
+    .src(['src/index.less'])
+    .pipe(less())
+    .pipe(rename('meltline.css'))
+    .pipe(gulp.dest('./'))
+    .pipe(cssmin())
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest('./'))
+    ;
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['src/**/*.less'], ['less']);
+});
+
+gulp.task('default', ['less', 'watch']);
