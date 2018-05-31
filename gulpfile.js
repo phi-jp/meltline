d8e7aa62e627028e603005da96080e6bf5014383
phi <phi1618jp@gmail.com>
2018-06-01 00:37:43 +0900

    build and version up

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,25 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require("gulp-rename"); 
 var cssmin = require('gulp-cssmin');
+var header  = require('gulp-header');
+var package = require('./package.json');
+var banner = [
+  "/*!",
+  " * <%= pkg.name %> <%= pkg.version %>",
+  " * <%= pkg.description %>",
+  " * MIT Licensed",
+  " * ",
+  " * Copyright (C) 2016 phi, http://meltlinecss.com/",
+  " */",
+  "",
+  "",
+].join('\n');
 
 gulp.task('less', function() {
   gulp
     .src(['src/index.less'])
     .pipe(less())
+    .pipe(header(banner, { pkg: package, }))
     .pipe(rename('meltline.css'))
     .pipe(gulp.dest('./'))
     .pipe(cssmin())
